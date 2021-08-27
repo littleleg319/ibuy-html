cb7de10e884fe767c75facd4e31a17bf590174c2
Paola <Paola@DESKTOP-2QSLDP0>
2021-08-28 00:14:12 +0200

    UserDAO except update visualizazions

--- TIW-project5-HTML/src/main/java/ibuy/html/dao/UserDAO.java
+++ TIW-project5-HTML/src/main/java/ibuy/html/dao/UserDAO.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import ibuy.html.beans.User;
+import ibuy.html.beans.Product;
 
 public class UserDAO {
 	private Connection con;
@@ -35,5 +38,53 @@ public class UserDAO {
 				}
 			}
 		}
+	}
+	
+	public List<Product> findLastProductByUser(String userid) throws SQLException {
+		List<Product> prods = new ArrayList<Product>();
+		String check = "SELECT count(*) as total FROM user_product  WHERE userid = ?";
+		try (PreparedStatement ps = con.prepareStatement(check);){
+			ps.setString(1, userid);
+			ResultSet res = ps.executeQuery();
+			if (res.isBeforeFirst())
+				return null;
+			else {
+		int s = res.getInt("total");
+		if ( s >= '5') {
+		String query1 = "SELECT code, name FROM product INNER JOIN user_product ON product.code = user_product.productid  WHERE user_product.userid = ? ";
+		try (PreparedStatement pstatement = con.prepareStatement(query1);) {
+			pstatement.setString(1, userid);
+			try (ResultSet result = pstatement.executeQuery();) {
+				if (!result.isBeforeFirst()) // no results, credential check failed
+					return null;
+				else {
+					while(result.next()) {
+					Product prod = new Product();
+					prod.setCode(result.getString("code"));
+					prod.setName(result.getString("name"));
+					prods.add(prod);
+					}
+				}
+			}
+		}
+		}
+	}
+}
+		return prods;
+}
+
+	public int findmissingProd(String userid) throws SQLException {
+		int s = 0;
+		String check = "SELECT count(*) as total FROM user_product  WHERE userid = ?";
+		try (PreparedStatement ps = con.prepareStatement(check);){
+			ps.setString(1, userid);
+			ResultSet res = ps.executeQuery();
+			if (res.isBeforeFirst())
+				return s;
+			else {
+					res.getInt("total");
+			}
+		}
+		return s;
 	}
 }
